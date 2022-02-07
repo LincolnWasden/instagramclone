ced59ac327ff9ac3bf750ac0ce52aecb9074a506
Lincoln Wasden <lincolndwasden@gmail.com>
2022-02-07 16:09:20 -0700

    commit

--- firebase.js
+++ firebase.js
@@ -1,24 +1,25 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps, getApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { initializeApp, getApp, getApps } from "firebase/app";
 
+import {getFirestore} from "firebase/firestore";
+import {getStorage} from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAfIKXQr0TCVsXTglYEFBkePFeKQOQjT64",
-  authDomain: "insta-2-24a39.firebaseapp.com",
-  projectId: "insta-2-24a39",
-  storageBucket: "insta-2-24a39.appspot.com",
-  messagingSenderId: "1094841440758",
-  appId: "1:1094841440758:web:c01750a7adf69e20210fd7",
+  apiKey: "AIzaSyCTvOV5tOs-7UlZuK6iAD2X2F8H5UsGipc",
+  authDomain: "whatsnewwiththis.firebaseapp.com",
+  projectId: "whatsnewwiththis",
+  storageBucket: "whatsnewwiththis.appspot.com",
+  messagingSenderId: "894247825032",
+  appId: "1:894247825032:web:676010e700b853f1dd393d"
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore();
 const storage = getStorage();
 
-export { app, db, storage };
+
+export {app, db, storage};
